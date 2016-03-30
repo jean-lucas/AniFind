66707540a7aea33c43521e58357e07b5afa64caf
Mathew Hobers <hobersme@mcmaster.ca>
2016-03-30 18:08:20 -0400

    Added compareTo method to Result adt and fixed its toString method

--- app/src/main/java/se3a04/anifind/Result.java
+++ app/src/main/java/se3a04/anifind/Result.java
@@ -10,28 +10,38 @@ import se3a04.anifind.DataEntities.Animal;
  */
 public class Result {
 
-    private ArrayList<String> answers;
-    private Animal selectedAnimal;
+    private ArrayList<String> answers;  //List of user's answers associated with the selected animal
+    private String selectedAnimal;  //The name of the user selected animal
 
-    public Result(ArrayList<String> answers, Animal selectedAnimal){
+    public Result(ArrayList<String> answers, String selectedAnimalName){
         this.answers = answers;
-        this.selectedAnimal = selectedAnimal;
+        this.selectedAnimal = selectedAnimalName;
     }
 
     public ArrayList<String> getAnswers(){
         return answers;
     }
 
-    public Animal getAnimal(){
+    public String getAnimalName(){
         return selectedAnimal;
+    }
+
+    public boolean compareTo(Result otherResult){
+        boolean equal = false;
+        if (this.answers.equals(otherResult) && this.selectedAnimal.equals(otherResult.selectedAnimal))
+            equal = true;
+        return equal;
     }
 
     public String toString(){
         String resultsOutput = "";
+
+        resultsOutput += selectedAnimal;
+
         for (String s : answers){
-            resultsOutput += s + ",";
+            resultsOutput += "," + s;
         }
-        resultsOutput += selectedAnimal.getName();
+
         return resultsOutput;
     }
 }
