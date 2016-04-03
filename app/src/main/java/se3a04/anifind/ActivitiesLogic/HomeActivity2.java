4caa8a5b2f93053d5f7145e2b52204447c7bd451
manthaar <akshay.mantha@gmail.com>
2016-04-03 17:35:26 -0400

    add colour, music

--- app/src/main/java/se3a04/anifind/ActivitiesLogic/HomeActivity2.java
+++ app/src/main/java/se3a04/anifind/ActivitiesLogic/HomeActivity2.java
@@ -1,9 +1,9 @@
 package se3a04.anifind.ActivitiesLogic;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -48,5 +48,31 @@ public class HomeActivity2 extends AppCompatActivity {
 
         finish();
 
+    }
+
+    public class BackgroundSound extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            MediaPlayer player = MediaPlayer.create(HomeActivity2.this, R.raw.nature);
+            player.setLooping(true); // Set looping
+            player.setVolume(1.0f, 1.0f);
+            player.start();
+
+            return null;
+        }
+
+    }
+
+    BackgroundSound bBackgroundSound = new BackgroundSound();
+
+    public void onResume() {
+        super.onResume();
+        bBackgroundSound.execute((Void[]) null);
+    }
+
+    public void onPause() {
+        super.onPause();
+        bBackgroundSound.cancel(true);
     }
 }
